80435f575e8137b15d6dafe637ce2fb9e25931a5
e.muhamethanov <eldarmu.1m@gmail.com>
2024-11-05 19:28:40 +0300

    Поправил ошибку сборки

--- src/editorLayout/view/EditorLayout.tsx
+++ src/editorLayout/view/EditorLayout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import { Content } from "antd/es/layout/layout";
 import { Layout } from "antd";
 import { MarkdownEditorLayout } from "./markdownEditor/MarkdownEditorLayout";
